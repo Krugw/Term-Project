302aae84429bba60006f692d5c3c0c7ed6b4ad33
GabeStep <stepanog@mail.gvsu.edu>
2015-02-12 19:37:52 -0500

    Added some of the stuff Haltson made to main project
    #420blazeitup
    Also I attempted to make to attempt to avoid some errors and make it work better

--- GUI.java
+++ GUI.java
@@ -8,26 +8,43 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 public class GUI extends JFrame implements ActionListener {
+	private JMenuBar menus;
+	private JMenu fileMenu;
+	private JMenu actionMenu;
+
+	// fileMenu
+	private JMenuItem LoadItem;
+	private JMenuItem New_project;
+	private JMenuItem exitItem;
+	private JMenuItem saveItem;
+
+	// account Menu
+	private JMenuItem AddUseCase;
+	private JMenuItem editUseCase;
+	private JMenuItem helpUseCase;
+
 	private JFrame frame;
-	private JButton create,new_project,save,edit,load;
-	private JPanel panel,panel2,panel_1;
-	private JLabel success_input,minimal_input,alternativeFlow_input;
+	private JButton new_project, edit, load;
+	private JPanel panel, panel2, panel_1;
+
+	private JLabel success_input, minimal_input, alternativeFlow_input;
 	private JLabel primaryFlow_input, preconditions_input, triggers_input;
-	private JLabel prim_actors_input,description_input,ID_input;
+	private JLabel prim_actors_input, description_input, ID_input;
 	private JLabel name_input;
+
 	private Project CurrentProject = new Project();
 	private UseCaseEditor UCE;
 	private UseCase CurrentUseCase;
-	private CreateDialog Dialog;
 	private ArrayList<String> ids;
 	private String file;
-	private JToolBar toolBar;
+
+	private CreateDialog Dialog;
 	private JComboBox<UseCase> ComboBox;
-	
+
 	private LoadFileBox loadFile;
 
 	public static void main(String[] args) {
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -39,50 +56,79 @@ public class GUI extends JFrame implements ActionListener {
 			}
 		});
 	}
- public GUI(){
-	 initialize();
- }
-  public void UCE_Utility(){
-		 UCE.addSaveListener(new ActionListener(){
-	         public void actionPerformed(ActionEvent e) {
-	        	 UseCase uc = UCE.getUC();
-	        	 System.out.println(uc.getPreconditions());
-	             save(uc);
-	         }
-	     });
-  }
+
+	public GUI() {
+		initialize();
+	}
+
+	public void UCE_Utility() {
+		UCE.addSaveListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UseCase uc = UCE.getUC();
+				System.out.println(uc.getPreconditions());
+				save(uc);
+			}
+		});
+	}
+
 	private void initialize() {
+		/** File menu */
+		fileMenu = new JMenu("File");
+		LoadItem = new JMenuItem("Load");
+		New_project = new JMenuItem("New Project");
+		exitItem = new JMenuItem("Exit");
+		saveItem = new JMenuItem("Save");
+
+		fileMenu.add(LoadItem);
+		fileMenu.add(New_project);
+		fileMenu.add(saveItem);
+		fileMenu.add(exitItem);
+
+		saveItem.addActionListener(this);
+		New_project.addActionListener(this);
+		LoadItem.addActionListener(this);
+		exitItem.addActionListener(this);
+
+		/** action menu */
+		actionMenu = new JMenu("Action");
+		AddUseCase = new JMenuItem("New Usecase");
+		editUseCase = new JMenuItem("Edit");
+		helpUseCase = new JMenuItem("Help");
+		
+		AddUseCase.addActionListener(this);
+		editUseCase.addActionListener(this);
+		helpUseCase.addActionListener(this);
+		
+		actionMenu.add(AddUseCase);
+		actionMenu.add(editUseCase);
+		actionMenu.add(helpUseCase);
+
 		frame = new JFrame();
 		frame.setBounds(200, 200, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		/** Menu bar */
+		menus = new JMenuBar();
 		ids = new ArrayList<String>();
-		
+
 		panel_1 = new JPanel();
 		panel_1.setLayout(new GridBagLayout());
-        	GridBagConstraints constraints = new GridBagConstraints();
-        	constraints.gridwidth = 10;
-        	
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridwidth = 10;
+
 		frame.getContentPane().add(panel_1, BorderLayout.WEST);
 		panel_1.setLayout(new GridLayout(10, 2, 0, 0));
 
-		toolBar = new JToolBar();
-		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
-
-		save = new JButton("Save");
-		toolBar.add(save);
-		save.addActionListener(this);
+		frame.getContentPane().add(menus, BorderLayout.NORTH);
 		load = new JButton("Load");
 		load.addActionListener(this);
 
 		new_project = new JButton("New Project");
 		new_project.addActionListener(this);
-		
-		create = new JButton("New Use Case");
-		create.addActionListener(this);
-		
+
 		panel2 = new JPanel();
 		frame.getContentPane().add(panel2, BorderLayout.CENTER);
-		GridLayout g1_panel = new GridLayout(3,4);
+		GridLayout g1_panel = new GridLayout(3, 4);
 		g1_panel.setVgap(10);
 		g1_panel.setHgap(10);
 		panel2.setLayout(g1_panel);
@@ -108,14 +154,13 @@ public class GUI extends JFrame implements ActionListener {
 		panel2.add(fill9);
 		JLabel fill = new JLabel();
 		panel2.add(fill);
-		
-		
+
 		panel = new JPanel();
 		panel.setVisible(false);
 
 		JLabel space = new JLabel();
 		panel.add(space);
-		
+
 		name_input = new JLabel("Name");
 		panel.add(name_input);
 
@@ -128,7 +173,7 @@ public class GUI extends JFrame implements ActionListener {
 		JLabel description = new JLabel();
 		description = new JLabel("Description");
 		panel.add(description);
-		
+
 		JScrollPane sp3 = new JScrollPane();
 		panel.add(sp3);
 
@@ -191,10 +236,10 @@ public class GUI extends JFrame implements ActionListener {
 
 		success_input = new JLabel("Success");
 		sp10.setViewportView(success_input);
-		
+
 		JPanel panel_2 = new JPanel();
 		frame.getContentPane().add(panel_2, BorderLayout.SOUTH);
-		
+
 		edit = new JButton("Edit");
 		edit.setVisible(false);
 		panel_2.add(edit);
@@ -210,37 +255,39 @@ public class GUI extends JFrame implements ActionListener {
 		createComboBox();
 		display();
 	}
-	public void display(){
+
+	public void display() {
 		panel.setVisible(true);
 		panel2.setVisible(false);
-		toolBar.add(create);
-		toolBar.add(load);
+		menus.add(fileMenu);
+		menus.add(actionMenu);
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		GridLayout g1_panel = new GridLayout(10, 2);
 		g1_panel.setVgap(2);
 		g1_panel.setHgap(2);
 		panel.setLayout(g1_panel);
-		if(CurrentUseCase != null){
-		success_input.setText(CurrentUseCase.getSuccessGuarantees());
-		minimal_input.setText(CurrentUseCase.getMinimalGuaruntees());
-		alternativeFlow_input.setText(CurrentUseCase.getAlternativeflow());
-		primaryFlow_input.setText(CurrentUseCase.getPrimaryflow());
-		preconditions_input.setText(CurrentUseCase.getPreconditions());
-		triggers_input.setText(CurrentUseCase.getTriggers());
-		prim_actors_input.setText(CurrentUseCase.getPrimaryActors());
-		description_input.setText(CurrentUseCase.getDescription());
-		ID_input.setText(CurrentUseCase.getID());
-		name_input.setText(CurrentUseCase.getName());
+		edit.setVisible(true);
+		if (CurrentUseCase != null) {
+			success_input.setText(CurrentUseCase.getSuccessGuarantees());
+			minimal_input.setText(CurrentUseCase.getMinimalGuaruntees());
+			alternativeFlow_input.setText(CurrentUseCase.getAlternativeflow());
+			primaryFlow_input.setText(CurrentUseCase.getPrimaryflow());
+			preconditions_input.setText(CurrentUseCase.getPreconditions());
+			triggers_input.setText(CurrentUseCase.getTriggers());
+			prim_actors_input.setText(CurrentUseCase.getPrimaryActors());
+			description_input.setText(CurrentUseCase.getDescription());
+			ID_input.setText(CurrentUseCase.getID());
+			name_input.setText(CurrentUseCase.getName());
 		}
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == create) {
+		if (e.getSource() == AddUseCase) {
 			UCE = new UseCaseEditor();
 			UCE.setVisible(true);
 			UCE_Utility();
 		}
-		if (e.getSource() == new_project) {
+		if (e.getSource() == new_project || e.getSource() == New_project) {
 			Dialog = new CreateDialog();
 			file = Dialog.getDirectory();
 			CurrentProject.setProjectName(Dialog.getFileName());
@@ -251,61 +298,60 @@ public class GUI extends JFrame implements ActionListener {
 			createComboBox();
 			display();
 		}
-		if (e.getSource() == save) {
-			if(CurrentUseCase != null)
-			save(CurrentUseCase);
+		if (e.getSource() == saveItem) {
+			if (CurrentUseCase != null)
+				save(CurrentUseCase);
 		}
-		
-		if (e.getSource() == edit) {
-				UCE = new UseCaseEditor();
+
+		if (e.getSource() == edit || e.getSource() == editUseCase) {
+			UCE = new UseCaseEditor();
+			if (CurrentUseCase != null) {
 				UCE.setUC(CurrentUseCase);
-				UCE.setVisible(true);
-				UCE_Utility();
+			} else {
+				UseCase uc = new UseCase();
+				UCE.setUC(uc);
+			}
+			UCE.setVisible(true);
+			UCE_Utility();
 		}
 		if (e.getSource() == ComboBox) {
-		if(ComboBox.getSelectedItem() != null)
-			CurrentUseCase = (UseCase) ComboBox.getSelectedItem();
+			if (ComboBox.getSelectedItem() != null)
+				CurrentUseCase = (UseCase) ComboBox.getSelectedItem();
 			display();
 		}
-		if (e.getSource() == load) {
+		/*if (helpUseCase == e.getSource()) {
+
+			JOptionPane.showMessageDialog(null,
+					"for future usage i guess\n Version 1.0",
+					"Version Information", JOptionPane.INFORMATION_MESSAGE);
+
+		}*/
+		if (e.getSource() == load || e.getSource() == LoadItem) {
 			loadFile = new LoadFileBox();
-			file=loadFile.getFileSelected();
+			file = loadFile.getFileSelected();
 			CurrentProject.loadFromXML(file);
 			ids = CurrentProject.Getids();
-			UseCase uc = CurrentProject.GetUsecase(ids.get(0));
-			
-			//uc.loadFromText("filename");
-			
-			CurrentUseCase = uc;
-			success_input.setText(uc.getSuccessGuarantees());
-			minimal_input.setText(uc.getMinimalGuaruntees());
-			alternativeFlow_input.setText(uc.getAlternativeflow());
-			primaryFlow_input.setText(uc.getPrimaryflow());
-			preconditions_input.setText(uc.getPreconditions());
-			triggers_input.setText(uc.getTriggers());
-			prim_actors_input.setText(uc.getPrimaryActors());
-			description_input.setText(uc.getDescription());
-			ID_input.setText(uc.getID());
-			name_input.setText(uc.getName());
+			if(!ids.isEmpty())
+			CurrentUseCase = CurrentProject.GetUsecase(ids.get(0));
 			createComboBox();
-			edit.setVisible(true);
 			display();
-			
+
 		}
 	}
+
 	public void createComboBox() {
 
-        Vector<UseCase> useCases = new Vector<UseCase>();
-        if(!ids.isEmpty()) {
-            for (int i = 0; i < ids.size(); i++) {
-                String id = ids.get(i);
-                useCases.add(CurrentProject.GetUsecase(id));
-            }
-        }
-        MyComboBoxModel myModel = new MyComboBoxModel(useCases);
-        ComboBox = new JComboBox<UseCase>(myModel);
-        ComboBox.addActionListener(this);
-        panel_1.removeAll();
-        panel_1.add(ComboBox,BorderLayout.WEST);
-    }
+		Vector<UseCase> useCases = new Vector<UseCase>();
+		if (!ids.isEmpty()) {
+			for (int i = 0; i < ids.size(); i++) {
+				String id = ids.get(i);
+				useCases.add(CurrentProject.GetUsecase(id));
+			}
+		}
+		MyComboBoxModel myModel = new MyComboBoxModel(useCases);
+		ComboBox = new JComboBox<UseCase>(myModel);
+		ComboBox.addActionListener(this);
+		panel_1.removeAll();
+		panel_1.add(ComboBox, BorderLayout.WEST);
+	}
 }
