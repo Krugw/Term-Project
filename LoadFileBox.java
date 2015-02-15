bdc2c554578aecbbe396c642a152cf8ff9acbed1
lnsomnia <raddatzh@mail.gvsu.edu>
2015-02-15 14:02:06 -0800

    Added comments

--- LoadFileBox.java
+++ LoadFileBox.java
@@ -1,61 +1,84 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.File;
+import javax.swing.*;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+/*****************************************************************
+LoadFileBox creates and manages a custom JFileChooser for use by the
+other parts of the GUI
 
+@authors 
+@version Winter 2015
+*****************************************************************/
 public class LoadFileBox implements ActionListener {
 	private JFileChooser fileChooser;
 	private JDialog popUp;
 	private String selectedFile;
 
-	public LoadFileBox() {
-		fileChooser = new JFileChooser();
-		// fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		fileChooser.setCurrentDirectory(new File(System
-				.getProperty("user.home")));
-		int result = fileChooser.showOpenDialog(popUp);
+    /*****************************************************************
+	Instantiates a JfileChooser and sets default directory
+	to the user.home. Displays absolute path and allows user
+	to make next operation.
+    *****************************************************************/
+	public LoadFileBox() {		
+		
+	fileChooser = new JFileChooser();
+	fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+	int result = fileChooser.showOpenDialog(popUp);
+	
 		if (result == JFileChooser.APPROVE_OPTION) {
-			// user selects a file
-
+			
 			selectedFile = fileChooser.getSelectedFile().getAbsolutePath();
-			System.out.println(selectedFile);
+			System.out.println(selectedFile);			
 		}
+		
 		JPanel messagePane = new JPanel();
-		messagePane.add(new JLabel("Choose File: "));
-
-		// popUp.setVisible(true);
+		messagePane.add(new JLabel("Choose File: "));		
+		popUp.setLocationRelativeTo(null);
 	}
 
-	public LoadFileBox(boolean create) {
+    /*****************************************************************
+    Instantiates a custom JfileChooser 
+    @param create - boolean request for creation of LoadFileBox
+    *****************************************************************/
+	public LoadFileBox(boolean create) {		
+		
 		fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		fileChooser.setCurrentDirectory(new File(System
-				.getProperty("user.home")));
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		
 		int result = fileChooser.showOpenDialog(popUp);
 		if (result == JFileChooser.APPROVE_OPTION) {
-			// user selects a file
+			
+			/** gets absolute path of operators chosen file **/
 			selectedFile = fileChooser.getSelectedFile().getAbsolutePath();
 			System.out.println(selectedFile);
 		}
-		JPanel messagePane = new JPanel();
+		
+		JPanel messagePane = new JPanel();	
 		messagePane.add(new JLabel("Choose File: "));
 
-		// popUp.setVisible(true);
-	}
 
+	}
+    /*****************************************************************
+    Returns absolute path of users desired file
+    @return selectedFile - String representation of absolute path
+    *****************************************************************/
 	public String getFileSelected() {
 		return selectedFile;
 	}
 
+	/**************************************************************
+	 Manages the action listeners that are currently connected to
+	 GUI objects.
+	 @param e the event
+	**************************************************************/
 	public void actionPerformed(ActionEvent e) {
 
 	}
-
+	
+	/*****************************************************************
+	main method, creates the instance of LoadFileBox()
+	*****************************************************************/	
 	public static void main(String[] args) {
 		LoadFileBox LBF = new LoadFileBox();
 
