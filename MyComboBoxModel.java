8e949bd41e8a2e6546e1542d2f72109ca366b328
lnsomnia <raddatzh@mail.gvsu.edu>
2015-02-16 10:37:54 -0500

    Added comments

--- MyComboBoxModel.java
+++ MyComboBoxModel.java
@@ -1,11 +1,25 @@
 import javax.swing.*;
 import java.util.Vector;
 
+
+/*****************************************************************
+MyComboBoxModel creates and manages a ComboBox to be used by other
+parts of our program. In its current form relies on UseCase.java;
+as it's contents are generated therein. Extends the DefaultComboBox
+class.
+@authors
+@version Summer 2015
+*****************************************************************/
 public class MyComboBoxModel extends DefaultComboBoxModel<UseCase> {
     public MyComboBoxModel(Vector<UseCase> useCases) {
         super(useCases);
     }
-    @Override
+    
+	/*****************************************************************
+	Returns the currently selected UseCase
+	@return selectedUSeCase
+	*****************************************************************/	
+    @Override    
     public UseCase getSelectedItem() {
         UseCase selectedUseCase = (UseCase) super.getSelectedItem();
         return selectedUseCase;
