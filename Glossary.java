b85a9da447837105febe8657ea9864923f6ec604
breckerm <mbrecker_1@yahoo.com>
2015-03-16 15:16:50 -0400

    Did checkstyle for R2

--- Glossary.java
+++ Glossary.java
@@ -1,28 +1,65 @@
-import java.util.ArrayList;
-
+/*********************************************************************
+ * This class creates a term and its definition for the 
+ * Glossary in the Usecase.
+ *
+ * @author Wesley Krug, Gabriel Steponovich,
+ * Michael Brecker, Halston Raddatz
+ * @version Winter 2015
+ ********************************************************************/
 public class Glossary {
+	
+	/**
+	 * This is the term name.
+	 */
 	private String word;
+	
+	/**
+	 * This is the term's definition.
+	 */
 	private String definition;
 	
 	
-	public Glossary(){
+	/**
+	 * This is the constructor that initializes the global variables.
+	 */
+	public Glossary() {
 		word = "";
 		definition = "";
 	}
 	
-	public void setWord(String word){
-		this.word = word;
+	/**
+	 * This sets the name of the term.
+	 * 
+	 * @param term is the String of the word name.
+	 */
+	public final void setWord(final String term) {
+		this.word = term;
 	}
 	
-	public void setDefinition(String definition){
-		this.definition = definition;
+	/**
+	 * This sets the definition for a term.
+	 * 
+	 * @param def is a string definition of the term.
+	 */
+	public final void setDefinition(final String def) {
+		this.definition = def;
 	}
 	
-	public String getWord(){
+	/**
+	 * This gets the name of a term.
+	 * 
+	 * @return the name of the term.
+	 */
+	public final String getWord() {
 		return word;
 	}
 	
-	public String getDefinition(){
+	/**
+	 * This gets the definition of the term.
+	 * 
+	 * @return the definition of the term.
+	 */
+	public final String getDefinition() {
 		return definition;
 	}
 	
