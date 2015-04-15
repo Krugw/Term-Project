bb8f9b2f38e7b2c51e974d1aca75728ef3628cec
GabeStep <stepanog@mail.gvsu.edu>
2015-04-15 15:36:54 -0400

    Fixed the opening of wrong files

--- LoadFileBox.java
+++ LoadFileBox.java
@@ -43,8 +43,11 @@ public class LoadFileBox implements ActionListener {
         int result = fileChooser.showOpenDialog(popUp);
 
         if (result == JFileChooser.APPROVE_OPTION) {
-
+            if (!fileChooser.getSelectedFile().getAbsolutePath().contains(".ucp")) {
+                JOptionPane.showMessageDialog(null, "Invalid file type.\n", "Error", JOptionPane.ERROR_MESSAGE);
+            }else{
             selectedFile = fileChooser.getSelectedFile().getAbsolutePath();
+            }
         }
 
         JPanel messagePane = new JPanel();
