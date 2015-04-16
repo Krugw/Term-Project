05e84749c018b476dbcda22b0ba5d6f439c5cb22
Krugw <Krugw@mail.gvsu.edu>
2015-04-16 13:43:09 -0400

    Update MyComboBoxModel.java

--- MyComboBoxModel.java
+++ MyComboBoxModel.java
@@ -1,37 +1,37 @@
 
+import java.util.ArrayList;
 import java.util.Vector;
 import javax.swing.DefaultComboBoxModel;
 
 
 /*****************************************************************
-MyComboBoxModel creates and manages a ComboBox to be used by other
-parts of our program. In its current form relies on UseCase.java;
-as it's contents are generated therein. Extends the DefaultComboBox
-class.
-authors Wesley Krug, Gabriel Steponovich, 
-         Michael Brecker, Halston Raddatz
-@version Winter 2015
-*****************************************************************/
-public class MyComboBoxModel extends DefaultComboBoxModel<UseCase> {
+ MyComboBoxModel creates and manages a ComboBox to be used by other
+ parts of our program. In its current form relies on UseCase.java;
+ as it's contents are generated therein. Extends the DefaultComboBox
+ class.
+ authors Wesley Krug, Gabriel Steponovich,
+ Michael Brecker, Halston Raddatz
+ @version Winter 2015
+ *****************************************************************/
+public class MyComboBoxModel extends DefaultComboBoxModel<PrimaryFlowStep> {
     /**
-	 * version id
-	 */
-	private static final long serialVersionUID = 1L;
-
-	/**
-     * @param useCases
+     * version id
      */
-    public MyComboBoxModel(final Vector<UseCase> useCases) {
-        super(useCases);
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param pFlowSteps
+     */
+    public MyComboBoxModel(final Vector<PrimaryFlowStep> pFlowSteps) {
+        super(pFlowSteps);
     }
-    
-	/*****************************************************************
-	Returns the currently selected UseCase.
-	@return selectedUSeCase
-	*****************************************************************/	
+
+    /*****************************************************************
+     Returns the currently selected UseCase.
+     @return selectedUSeCase
+     *****************************************************************/
     @Override
-	public final UseCase getSelectedItem() {
-        UseCase selectedUseCase = (UseCase) super.getSelectedItem();
-        return selectedUseCase;
+    public final PrimaryFlowStep getSelectedItem() {
+        return (PrimaryFlowStep) super.getSelectedItem();
     }
 }
